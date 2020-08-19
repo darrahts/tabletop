b1bbc96b4bfddf7b599a0d86d893098637a63fc3
darrahts <timothy.s.darrah@vanderbilt.edu>
2020-08-19 15:47:58 -0500

    updated robot variables

--- fcns/moveRobot.m
+++ fcns/moveRobot.m
@@ -1,4 +1,4 @@
-function robot = moveRobot(robot, direction, distance, map)
+function robot = moveRobot(robot, direction, distance, map, poi)
 %%
 %   updates the robots location 
 %
@@ -11,32 +11,60 @@ switch direction
     case '^'
         new = robot.location(2) + distance;
         if new <= length(map)
-            robot.location(2) = new;
-            robot.direction = direction;
+            loc = map(robot.location(1), new);
+            if strcmp(loc.type, map(poi.blocks(1,1), poi.blocks(1,2)).type) == 0
+                robot.location(2) = new;
+                robot.direction = direction;
+                robot.locationType = loc.type;
+                robot.cumulativeReward = robot.cumulativeReward + loc.reward;
+            else
+                %fprintf('invalid move.');
+            end
         end
     case 'v'
         new = robot.location(2) - distance;
         if new > 0
-            robot.location(2) = new;
-            robot.direction = direction;
+            loc = map(robot.location(1), new);
+            if strcmp(loc.type, map(poi.blocks(1,1), poi.blocks(1,2)).type) == 0
+                robot.location(2) = new;
+                robot.direction = direction;
+                robot.locationType = loc.type;
+                robot.cumulativeReward = robot.cumulativeReward + loc.reward;                
+            else
+                %fprintf('invalid move.');
+            end
         end
     case '>'
         new = robot.location(1) + distance;
         if new <= length(map(1,:))
-            robot.location(1) = new;
-            robot.direction = direction;
+            loc = map(new, robot.location(2));
+            if strcmp(loc.type, map(poi.blocks(1,1), poi.blocks(1,2)).type) == 0
+                robot.location(1) = new;
+                robot.direction = direction;
+                robot.locationType = loc.type;
+                robot.cumulativeReward = robot.cumulativeReward + loc.reward;        
+            else
+                %fprintf('invalid move.');
+            end
         end
     case '<'
         new = robot.location(1) - distance;
         if new > 0
-            robot.location(1) = new;
-            robot.direction = direction;
+            loc = map(new, robot.location(2));
+            if strcmp(loc.type, map(poi.blocks(1,1), poi.blocks(1,2)).type) == 0
+                robot.location(1) = new;
+                robot.direction = direction;
+                robot.locationType = loc.type;
+                robot.cumulativeReward = robot.cumulativeReward + loc.reward;        
+            else
+                %fprintf('invalid move.');
+            end
         end
     otherwise
-        disp('invalid move.');
+        msgbox('invalid move.');
 end 
 
-if map(robot.location).type == 
+ 
      
 
 end
