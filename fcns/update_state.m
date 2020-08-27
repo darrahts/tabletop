a7fe11e478d1ac27b5e6eafa65a8f4231111842b
darrahts <timothy.s.darrah@vanderbilt.edu>
2020-08-26 19:57:12 -0500

    updates state, need to add comments

---
+++ fcns/update_state.m
@@ -0,0 +1,33 @@
+function state = update_state(state, agent, map)
+
+state.location = agent.location;
+
+if agent.soc >= 84
+    state.battery = 1;
+elseif agent.soc >= 36
+    state.battery = 2;
+elseif agent.soc >= 18
+    state.battery = 3;
+else
+    state.battery = 4;
+end
+
+if strcmp(map(state.location(1), state.location(2)).label, 'O-1')
+    state.achieved1 = true;
+    state.reward = state.reward + 5;
+end
+
+if strcmp(map(state.location(1), state.location(2)).label, 'O-2')
+    state.achieved2 = true;
+    state.reward = state.reward + 10;
+end
+
+
+if state.achieved1 == true 
+    if isequal(state.location, [1,1])
+        state.reward = state.reward + 20;
+    end
+end
+    
+end
+
