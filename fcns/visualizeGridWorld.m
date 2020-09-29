39d90848c25c142a7aacf2c446408c3639a04577
darrahts <timothy.s.darrah@vanderbilt.edu>
2020-09-29 00:15:11 -0500

    updated plot to visualize reward, value, action, state, label

--- fcns/visualizeGridWorld.m
+++ fcns/visualizeGridWorld.m
@@ -1,15 +1,21 @@
-function f = visualizeGridWorld(fig, map, poi, state, agent)
+function f = visualizeGridWorld(fig, map, poi, state, agent, policy)
 %%
 %   draws the map, robot, and points of interest
 %
 %%
+
+if ~exist('policy', 'var')
+    policy = repmat(1,1,numel(map));
+end
+
 f = figure(fig); clf;
 height = length(map(1,:));
 width = length(map);
 f.Position = [0 0 800 400];
 rectangle('Position', [0 0 width height]);
 hold on;
-grid on;
+set(gca, 'YTick', 0:length(map(1,:)));
+set(gca, 'XTick', 0:length(map(:,1)));
 
 start = poi.start;
 objectives = poi.objectives;
@@ -18,22 +24,47 @@ blocks = poi.blocks;
 
 rectangle('Position', [start(1,1)-1, start(1,2)-1, 1, 1], 'FaceColor', 	'#4DBEEE');
 s = line(start(1,1)-1, start(1,2)-1, 'LineWidth', 4, 'Color', '#4DBEEE');
-text(start(1)-.6, start(2)-.5, "S");
+text(start(1)-.2, start(2)-.1, "S", 'FontSize', 16);
 
-for i=1:1%length(objectives(:,:))
+for i=1:length(objectives(:,1))
    rectangle('Position', [objectives(i,1)-1, objectives(i,2)-1, 1, 1], 'FaceColor', 	'#77AC30');
-   text(objectives(i,1)-.6, objectives(i,2)-.5, sprintf("O-%d", i));
+   text(objectives(i,1)-.3, objectives(i,2)-.1, sprintf("O-%d", i), 'FontSize', 16);
    o = line(objectives(i,1)-1, objectives(i,2)-1, 'LineWidth', 4, 'Color', '#77AC30');
 end
-for i=1:1%length(pitfalls(:,:))
+for i=1:length(pitfalls(:,1))
    rectangle('Position', [pitfalls(i,1)-1, pitfalls(i,2)-1, 1, 1], 'FaceColor', 	'#A2142F');
-   text(pitfalls(i,1)-.6, pitfalls(i,2)-.5, sprintf("P-%d", i));
+   text(pitfalls(i,1)-.3, pitfalls(i,2)-.1, sprintf("P-%d", i), 'FontSize', 16);
    p = line(pitfalls(i,1)-1, pitfalls(i,2)-1, 'LineWidth', 4, 'Color', '#A2142F');
 end
 
-for i=1:1%length(blocks(:,:))
+for i=1:length(blocks(:,1))
    rectangle('Position', [blocks(i,1)-1, blocks(i,2)-1, 1, 1], 'FaceColor', 	'#000000');
    b = line(blocks(i,1)-1, blocks(i,2)-1, 'LineWidth', 4, 'Color', '#000000');
+end
+
+num = 1;
+for i=1:length(map(1,:))
+    for j = 1:length(map(:,1))
+        text(j-.9, i-.1, sprintf("R: %.2f", map(num).reward));
+        text(j-.9, i-.3, sprintf("V: %.2f", map(num).value));
+        text(j-.9, i-.9, sprintf("s(%d)", num));
+        
+        if(policy(num) == 1)
+            % up
+            annotation('arrow', [(j/length(map(:,1))/1.5 +.05) (j/length(map(:,1))/1.5 + .05)], [((i-1)/3.5 + .15) ((i-1)/3.5 + .25)]);
+        elseif(policy(num) == 2)
+            % down
+            annotation('arrow', [(j/length(map(:,1))/1.5 +.05) (j/length(map(:,1))/1.5 + .05)], [((i-1)/3.5 + .25) ((i-1)/3.5 + .15)]);
+        elseif(policy(num) == 3)
+            % left
+            annotation('arrow', [(j/length(map(:,1))/1.5 +.035) (j/length(map(:,1))/1.5 + .08)], [((i-1)/3.5 + .2) ((i-1)/3.5 + .2)]);
+        else
+            % right
+            annotation('arrow', [(j/length(map(:,1))/1.5 +.035) (j/length(map(:,1))/1.5 + .08)], [((i-1)/3.5 + .2) ((i-1)/3.5 + .2)]);
+        end
+        
+        num = num + 1;
+    end
 end
 
 r = plot(agent.location(1) -.7, agent.location(2)-.7, agent.direction, 'MarkerFaceColor', '#D95319', 'MarkerEdgeColor', '#D95319');
