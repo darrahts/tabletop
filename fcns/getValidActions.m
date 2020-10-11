d21f594819d00448a4952f8b03865cf279547222
darrahts <timothy.s.darrah@vanderbilt.edu>
2020-10-10 21:20:46 -0500

    deleted commented lines

--- fcns/getValidActions.m
+++ fcns/getValidActions.m
@@ -34,23 +34,18 @@ function validActions = getValidActions(j, i, map, actions)
   % cannot go left / west in these states
     if(j == 1)
         validActions = erase(validActions, '<');
-        % text(j-.9, i-.6, sprintf("<"));
-        % plot(j-.9, i-.6, '<', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k', 'MarkerSize', 5);
     end
     % cannot go right / east in these states
     if(j == length(map(:,1)))
         validActions = erase(validActions, '>');
-        % plot(j-.1, i-.6, '>', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k', 'MarkerSize', 5);
     end
     % cannot go up / north in these states
     if(i == length(map(1,:)))
         validActions = erase(validActions, '^');
-        % plot(j-.45, i-.1, '^', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k', 'MarkerSize', 5);
     end
     % cannot go down / south in these states
     if(i == 1)
         validActions = erase(validActions, 'v');
-        % plot(j-.45, i-.9, 'v', 'MarkerFaceColor', 'k', 'MarkerEdgeColor', 'k', 'MarkerSize', 5);
     end 
     
     % cant move on block spaces
